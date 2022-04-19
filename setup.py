e903b3281f057f1bb4b0f7b785b3aba8a007dec1
Shohei Fujii <fujii.shohei@gmail.com>
2022-04-19 16:20:46 +0800

    add dependencies in setup.py

--- setup.py
+++ setup.py
@@ -2,4 +2,23 @@ from setuptools import setup
 
 setup(name='kp2d',
       version="1.0",
-      packages=['kp2d'])
+      packages=['kp2d'],
+      install_requires=[
+          'awscli',
+          'boto3',
+          'h5py',
+          'jupyter',
+          'keras',
+          'matplotlib',
+          'numpy',
+          'opencv-python-headless',
+          'pandas',
+          'path.py',
+          'pillow',
+          'pycuda',
+          'tensorboardx',
+          'termcolor',
+          'tqdm',
+          'wandb',
+          'yacs'],
+      )
